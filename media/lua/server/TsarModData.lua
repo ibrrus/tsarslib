3510486368e230b705dcbd25f24c8e411786cbba
ibrrus <v.i.kolesnikov@hotmail.com>
2022-02-13 12:09:06 +0300

    Добавлен тестовый функционал для работы анимаций.
    Added test functionality for animations.

---
+++ media/lua/server/TsarModData.lua
@@ -0,0 +1,31 @@
+local tsarslibOnInitGlobalModData = function(_module, _packet)
+	if not ModData.exists("tsaranimations") then
+		local t = ModData.create("tsaranimations")
+	end
+end
+
+local tsarslibOnReceiveGlobalModData = function(_module, _packet)
+	if _module ~= "tsaranimations" then return; end;
+	-- print("SERVER" .. _module)
+	if (not _packet) then
+		print("aborted OnReceiveGlobalModData in trueClient "
+				.. (_packet or "missing _packet."));
+	else
+        -- if isClient() then
+            -- print("tsarslib ModData isClient")
+        -- elseif isServer() then
+            -- print("tsarslib ModData isServer")
+        -- else
+            -- print("tsarslib ModData local")
+        -- end
+        -- for i, k in pairs(_packet) do
+            -- print("tsaranimations")
+            -- print(i)
+            -- print(k)
+        -- end
+		ModData.add(_module, _packet);
+	end;
+end
+
+Events.OnInitGlobalModData.Add(tsarslibOnInitGlobalModData);
+Events.OnReceiveGlobalModData.Add(tsarslibOnReceiveGlobalModData);
